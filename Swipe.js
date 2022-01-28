b8c34d0411520f67ed03071605309d9db69fd7ae
ankit2023 <57229993+ankit2023@users.noreply.github.com>
2022-01-28 13:12:19 +0530

    Add files via upload

---
+++ Swipe.js
@@ -0,0 +1,9 @@
+var num1 = 55;
+var num2 = 20;
+var temp = 0;
+
+document.write("<br>Before Swap, num1 = " +num1+" num2 = "+num2);
+temp = num1;
+num1 = num2;
+num2 = temp;
+document.write("<br>After Swap, num1 = " +num1+" num2 = "+num2);
